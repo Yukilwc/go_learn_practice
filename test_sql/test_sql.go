cebfb5c888070f0a664edefe5414d3a8ac396f15
Misaka <misaka1033@gmail.com>
2023-01-30 10:47:48 +0800

    u

--- test_sql/test_sql.go
+++ test_sql/test_sql.go
@@ -11,7 +11,14 @@ var db *sql.DB
 
 func main() {
 	initDB()
-	doQuery()
+	// doQuery()
+	// doQueryMulti()
+	// name, err := inputName()
+	// fmt.Println(" inputName name", name)
+	// if err == nil {
+	// 	doInsert(name)
+	// }
+	updateRow()
 	defer db.Close()
 }
 func initDB() {
@@ -32,6 +39,7 @@ type User struct {
 	name string
 }
 
+// 单行查询
 func doQuery() {
 	var u User
 	sqlStr := "select * from user where id=?"
@@ -41,4 +49,87 @@ func doQuery() {
 		return
 	}
 	fmt.Printf("id:%d name:%s\n", u.id, u.name)
+}
+
+// 多行查询
+func doQueryMulti() {
+	sqlStr := "select * from user where id>?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+
+	}
+	defer rows.Close()
+	var userList []User
+	for rows.Next() {
+		var u User
+		rows.Scan(&u.id, &u.name)
+		userList = append(userList, u)
+	}
+	fmt.Println("user list", userList)
+}
+
+// 插入
+func doInsert(name string) {
+	sqlStr := "insert into user(name) values (?)"
+	ret, err := db.Exec(sqlStr, name)
+	if err != nil {
+		fmt.Println("insert failed", err)
+		return
+	}
+	theID, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Println("LastInsertId failed", err)
+	}
+	fmt.Println("insert success", theID)
+}
+func inputName() (string, error) {
+	var name string
+	fmt.Println("请输入名字:")
+	n, err := fmt.Scanln(&name)
+	if err != nil {
+		fmt.Errorf("scan in error:%s", err)
+		return "", err
+	}
+	fmt.Println("scan in name", n)
+	return name, nil
+}
+
+// 更新
+func updateRow() {
+	var id int
+	var name string
+	fmt.Println("请输入id和名字,用空格隔开，输入完成后回车:")
+	_, err := fmt.Scanln(&id, &name)
+	if err != nil {
+		fmt.Println("scan in error", err)
+		return
+	}
+	doUpdate(id, name)
+}
+func doUpdate(id int, name string) {
+	sqlStr := "update user set name=? where id=?"
+	ret, err := db.Exec(sqlStr, name, id)
+	if err != nil {
+		fmt.Println("exec error", err)
+		return
+	}
+	n, err := ret.RowsAffected()
+	if err == nil {
+		fmt.Println("rows n", n)
+	}
+}
+
+func doDelete(id int) {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, id)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
 }
