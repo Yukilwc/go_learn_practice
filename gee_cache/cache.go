74442a57235137f313025b59d86dd6bc14983f87
Misaka <misaka1033@gmail.com>
2023-02-01 18:03:57 +0800

    u

--- gee_cache/cache.go
+++ gee_cache/cache.go
@@ -1,1 +1,34 @@
 package geecache
+
+import (
+	"go_test_init/gee_cache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+// 增加了锁的add
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+// 增加了锁的get
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
