aeb7bcf5d011a66c3440264c0fac5197fe27292f
Misaka <misaka1033@gmail.com>
2023-02-02 18:00:50 +0800

    u

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_test_init/geecache"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
 
 func main() {
 	fmt.Println("hello,golang!")
+	geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(func(key string) ([]byte, error) {
+		log.Println("[SlowDB] search key ", key)
+		if v, ok := db[key]; ok {
+			return []byte(v), nil
+		}
+		return nil, fmt.Errorf("%s not exist", key)
+	}))
+	addr := "localhost:9999"
+	peers := geecache.NewHTTPPool(addr)
+	log.Println("geecache is running at ", addr)
+	http.ListenAndServe(addr, peers)
 }
