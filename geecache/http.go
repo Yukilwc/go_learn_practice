9508180289e22bf6f686c98949af043ce401197f
Misaka <misaka1033@gmail.com>
2023-02-02 17:31:33 +0800

    u

--- geecache/http.go
+++ geecache/http.go
@@ -1,1 +1,56 @@
 package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self, // 自身的地址
+		basePath: defaultBasePath,
+	}
+}
+
+// 添加个日志方法
+
+func (p *HTTPPool) Log(format string, v ...any) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		// 没按照前缀来
+		panic("HTTPPool serving unexpectd path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// <basepath>/<groupname>/<key>
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	groupName := parts[0]
+	key := parts[1]
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group "+groupName, http.StatusNotFound)
+		return
+	}
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-steam")
+	w.Write(view.ByteSlice())
+}
