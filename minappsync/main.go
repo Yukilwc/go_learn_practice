e1389445e7bd5324e81dba9dddf84eec672d8a8c
Misaka <misaka1033@gmail.com>
2023-02-13 13:17:42 +0800

    u

--- minappsync/main.go
+++ minappsync/main.go
@@ -5,12 +5,41 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("小程序同步Start")
-	var i int
-	fmt.Scan(i)
-	time.Sleep(33333333)
+	// reader := bufio.NewReader(os.Stdin)
+	// text, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("text:", text)
+	// fmt.Scanln(&text)
+	// fmt.Println("text scanln:", text)
+	// fmt.Scanln(&text)
+	// fmt.Println("text scanln:", text)
+	loop()
+}
+
+var mpFolder = "D:/workspace/work/web/gitee/etranscode/etransmp3IM"
+var backupFolder = "D:/workspace/work/web/gitee/etranscode/etransmp3Backup"
+
+func loop() {
+	var text string
+	for {
+		fmt.Println("请输入你的转移目标(mp/backup):", text)
+		fmt.Scanln(&text)
+		if text == "exit" || text == "quit" {
+			break
+		}
+	}
+}
+
+func mp2backup() {
+
+}
+
+func backup2mp() {
+
 }
