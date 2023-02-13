0a7291f4f434235d4d4f0f228cee8e18a5ff3356
Misaka <misaka1033@gmail.com>
2023-02-13 11:40:04 +0800

    u

--- minappsync/main.go
+++ minappsync/main.go
@@ -1,1 +1,16 @@
 package main
+
+// 编译指令
+// go build -o ./minappsync/minappsync.exe  ./minappsync/main.go
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("小程序同步Start")
+	var i int
+	fmt.Scan(i)
+	time.Sleep(33333333)
+}
