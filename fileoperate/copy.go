dc08b718d408c6bd985f53aecec0e4d2073d0c00
Misaka <misaka1033@gmail.com>
2023-02-13 11:19:27 +0800

    u

--- fileoperate/copy.go
+++ fileoperate/copy.go
@@ -1,8 +1,10 @@
 package fileoperate
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path"
 )
 
 func CopyFile(src, dst string) error {
@@ -29,4 +31,38 @@ func CopyFile(src, dst string) error {
 		return err
 	}
 	return os.Chmod(dst, srcInfo.Mode())
+}
+
+func CopyDir(src, dst string) error {
+	var err error
+	var fileList []os.DirEntry
+	var srcInfo os.FileInfo
+	// 先看看源文件夹是否存在
+	if srcInfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	// 参考源文件夹权限，创建目标文件夹
+	if err = os.MkdirAll(dst, srcInfo.Mode()); err != nil {
+		return err
+	}
+	//读取目录下的内容，获取全部文件/文件夹fileinfo切片
+	if fileList, err = os.ReadDir(src); err != nil {
+		return err
+	}
+	// 递归拷贝文件
+	for _, file := range fileList {
+		srcFilePath := path.Join(src, file.Name())
+		dstFilePath := path.Join(dst, file.Name())
+		if file.IsDir() {
+			// 当前遍历到的是一个文件夹 递归
+			if err = CopyDir(srcFilePath, dstFilePath); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err := CopyFile(srcFilePath, dstFilePath); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
 }
--- fileoperate/copy_test.go
+++ fileoperate/copy_test.go
@@ -6,6 +6,14 @@ func TestCopyFile(t *testing.T) {
 	src := "D:/workspace/libiary/ForTest/go_code/go_test_init/fileoperate/test/src/srcData.js"
 	dst := "D:/workspace/libiary/ForTest/go_code/go_test_init/fileoperate/test/dst/srcData.js"
 	if err := CopyFile(src, dst); err != nil {
-		t.Fatalf("copy failed error:%s", err)
+		t.Fatalf("copy file failed error:%s", err)
+	}
+}
+
+func TestCopyDir(t *testing.T) {
+	src := "D:/workspace/libiary/ForTest/go_code/go_test_init/fileoperate/test/srcFolder"
+	dst := "D:/workspace/libiary/ForTest/go_code/go_test_init/fileoperate/test/dstFolder"
+	if err := CopyDir(src, dst); err != nil {
+		t.Fatalf("copy dir failed error:%s:", err)
 	}
 }
